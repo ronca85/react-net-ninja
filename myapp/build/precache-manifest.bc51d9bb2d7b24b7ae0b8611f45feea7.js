395798f92bf2706525c02d619ff3e00fc7c75d53
ronca85 <ronik@hi.t-com.hr>
2019-06-14 10:51:18 +0200

    06-outputting-lists

--- myapp/build/precache-manifest.bc51d9bb2d7b24b7ae0b8611f45feea7.js
+++ myapp/build/precache-manifest.bc51d9bb2d7b24b7ae0b8611f45feea7.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "d87fe2879cc0f0af6959",
-    "url": "/static/js/main.e8e053bf.chunk.js"
+    "revision": "4890f4198fc1280c87ff",
+    "url": "/static/js/main.026036d3.chunk.js"
   },
   {
     "revision": "9eee67cf782a38d35aeb",
     "url": "/static/js/2.d6a534e5.chunk.js"
   },
   {
-    "revision": "d87fe2879cc0f0af6959",
+    "revision": "4890f4198fc1280c87ff",
     "url": "/static/css/main.978078ea.chunk.css"
   },
   {
-    "revision": "227ebf0572adbc1afe52b9430e99d3a0",
+    "revision": "a270dd57d5f82858b7304f580ea57cb0",
     "url": "/index.html"
   }
 ];
--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -2,16 +2,22 @@ import React, { Component } from 'react';
 
 class Ninjas extends Component {
 	render() {
-		// we get this.props automatically on class based components
-		// console.log(this.props);
-
 		// the line below creates three variables and stores corresponding props under them
-		const { name, age, belt } = this.props;
+		const { ninjas } = this.props;
+
+		const ninjaList = ninjas.map(ninja => {
+			return (
+				<div className="ninja" key={ ninja.id }>
+					<div>Name: { ninja.name }</div>
+					<div>Age: { ninja.age }</div>
+					<div>Belt: { ninja.belt }</div>
+				</div>
+			)
+		});
+
 		return (
-			<div className="ninja">
-				<div>Name: { name }</div>
-				<div>Age: { age }</div>
-				<div>Belt: { belt }</div>
+			<div className="ninja-list">
+				{ ninjaList }
 			</div>
 		)
 	}
