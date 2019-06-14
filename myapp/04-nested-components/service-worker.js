976a6064024538095dd387dacc03f39ed2140dc5
ronca85 <ronik@hi.t-com.hr>
2019-06-14 10:12:13 +0200

    04. nested components

--- myapp/04-nested-components/service-worker.js
+++ myapp/04-nested-components/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.469916d81685e57947d10f45ffa1f58d.js"
+  "./precache-manifest.469916d81685e57947d10f45ffa1f58d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
