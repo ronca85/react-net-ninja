e7f0f93d9218f485d6bf04c1e2e87af1c0ebf60d
ronca85 <ronik@hi.t-com.hr>
2019-06-14 12:03:23 +0200

    09. form inputs logging to console

---
+++ myapp/build/precache-manifest.1382ea1e6f16710940e73a0548289671.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7c17d5c5c4d8de6b92e4",
+    "url": "/static/js/main.fec378ea.chunk.js"
+  },
+  {
+    "revision": "e90574d74274c7fb4d49",
+    "url": "/static/js/2.c39fed63.chunk.js"
+  },
+  {
+    "revision": "7c17d5c5c4d8de6b92e4",
+    "url": "/static/css/main.978078ea.chunk.css"
+  },
+  {
+    "revision": "4153a0e1874ebd9af38fcfdf7bdf6dbd",
+    "url": "/index.html"
+  }
+];
