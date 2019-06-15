a90ea2b74085e98a3e2417efdf4b2f7a7f6ee539
ronca85 <ronik@hi.t-com.hr>
2019-06-15 15:27:42 +0200

    todoapp

---
+++ todoapp/build/precache-manifest.e434fe77d44c8dbb49324eaa92107dbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d5e0b45ad4399cdd8b70",
+    "url": "/static/js/main.1c35b3e6.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "d5e0b45ad4399cdd8b70",
+    "url": "/static/css/main.d7bc465c.chunk.css"
+  },
+  {
+    "revision": "1bb19566b55e184234e30a6f1679cc03",
+    "url": "/index.html"
+  }
+];
